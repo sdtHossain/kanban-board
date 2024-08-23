c31c37d738c750cff7ae4d4c5662be95c35913cb
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2024-08-24 03:42:38 +0600

    added html project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,35 @@ import TheBanner from '@/components/home/TheBanner.vue'
 
 <template>
   <main>
-    <TheBanner />
+    <div class="container py-5">
+      <h2 class="text-center mb-4">Kanban Board</h2>
+      <div class="row">
+        <!-- To Do Column -->
+        <div class="col-lg-4">
+          <div class="kanban-column">
+            <div class="kanban-header">To Do</div>
+            <div class="kanban-item">Task 1: Research</div>
+            <div class="kanban-item">Task 2: Planning</div>
+            <div class="kanban-item">Task 3: Design</div>
+          </div>
+        </div>
+        <!-- In Progress Column -->
+        <div class="col-lg-4">
+          <div class="kanban-column">
+            <div class="kanban-header">In Progress</div>
+            <div class="kanban-item">Task 4: Development</div>
+            <div class="kanban-item">Task 5: Testing</div>
+          </div>
+        </div>
+        <!-- Done Column -->
+        <div class="col-lg-4">
+          <div class="kanban-column">
+            <div class="kanban-header">Done</div>
+            <div class="kanban-item">Task 6: Review</div>
+            <div class="kanban-item">Task 7: Deployment</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
